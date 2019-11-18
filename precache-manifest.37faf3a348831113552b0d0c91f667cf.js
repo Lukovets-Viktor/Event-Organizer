483fb3b0e4327828817250be1a5332ebf962eaf3
Viktor Lukovets <nuerio63@gmail.com>
2019-11-18 19:34:00 +0200

    Updates

---
+++ precache-manifest.37faf3a348831113552b0d0c91f667cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3309fe97b7ab9c37803dfae28ba6f1",
+    "url": "/Event-Organizer/index.html"
+  },
+  {
+    "revision": "b6c40a9048f0af47c78f",
+    "url": "/Event-Organizer/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0779f9e626f08a3aad18",
+    "url": "/Event-Organizer/static/js/2.299186ce.chunk.js"
+  },
+  {
+    "revision": "b6c40a9048f0af47c78f",
+    "url": "/Event-Organizer/static/js/main.602c9d73.chunk.js"
+  },
+  {
+    "revision": "888a2b02d25e6735ec1d",
+    "url": "/Event-Organizer/static/js/runtime-main.f391ff62.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Event-Organizer/static/media/logo.25bf045c.svg"
+  }
+]);
